4c592dace1600422f32d7f0a4965e5451d43e37e
yh9-4 <69719142+yh9-4@users.noreply.github.com>
2022-11-01 19:49:29 +0800

    Update launch_distributed.sh

--- scripts/launch_distributed.sh
+++ scripts/launch_distributed.sh
@@ -1,5 +1,5 @@
 python ./distributed_launcher.py \
     --prepare_cmd="" \
     --aux_files=benchmark.py,network.py \
-    --cuda_visible_devices=""\
+    --cuda_visible_devices="0"\
     launcher.py \
